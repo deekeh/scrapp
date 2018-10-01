4e8651a493f6019b81eef55954cde156a80a6a58
Darshan Khot <43519784+deekeh@users.noreply.github.com>
2018-10-01 20:41:03 +0530

    Update sell.php

--- sell.php
+++ sell.php
@@ -8,14 +8,19 @@
 	else
 	{
 		$mail = $_SESSION['mail'];
-		$db = new PDO('mysql:host=hostname;dbname=scrappdb', "user", "password");
-		$stmt = $db->query('SELECT pincode FROM login where mail="' 		. $mail.		'"');
+		$db = new PDO('mysql:host=localhost;dbname=scrappdb', "root", "");
+		$stmt = $db->query('SELECT pincode FROM orders where mail="' 		. $mail.		'"');
 		$row = $stmt->fetch();
 		if(!empty($row['pincode']))	$pincode = $row['pincode'];
 
 		if (isset($_POST['sell-submit']) == true)
 		{
-			
+			$wt = $_POST['wt'];
+			$sql = "INSERT INTO orders (user_mail, mass) VALUES ('" .$mail."'," .$wt. ")";
+			echo $sql;
+			$db->beginTransaction();
+			$db->exec($sql);
+			$db->commit();
 			//echo "<script type='text/javascript'>window.alert('Applied successfully');</script>";
 			header('Location: index.php');
 			
