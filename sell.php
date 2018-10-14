c919e32f55070b2be3d509e61ec1579d1f95ebd8
darshan <darshankhot@protonmail.com>
2018-10-14 17:53:22 +0530

    added pincode session

--- sell.php
+++ sell.php
@@ -8,21 +8,23 @@
 	else
 	{
 		$mail = $_SESSION['mail'];
-		$db = new PDO('mysql:host=localhost;dbname=scrappdb', "root", "");
-		$stmt = $db->query('SELECT pincode FROM orders where mail="' 		. $mail.		'"');
-		$row = $stmt->fetch();
-		if(!empty($row['pincode']))	$pincode = $row['pincode'];
+		$db = new PDO('mysql:host=dk;dbname=scrappdb', "DK", "Abhidksrvs");
+		//$stmt = $db->query('SELECT pincode FROM login where mail="' 		. $mail.		'"');
+		//$row = $stmt->fetch();
+		//if(!empty($row['pincode']))	$pincode = $row['pincode'];
+		$pincode = $_SESSION['pincode'];
 
 		if (isset($_POST['sell-submit']) == true)
 		{
 			$wt = $_POST['wt'];
-			$sql = "INSERT INTO orders (user_mail, mass) VALUES ('" .$mail."'," .$wt. ")";
-			echo $sql;
+			if($_POST['wt-unit'] == 'kilograms') $wt = $wt * 1000;
+			$sql = "INSERT INTO orders (user_mail, mass, pincode) VALUES ('" .$mail."'," .$wt. ",'" .$pincode. "')";
+			//echo $sql;
 			$db->beginTransaction();
 			$db->exec($sql);
 			$db->commit();
 			//echo "<script type='text/javascript'>window.alert('Applied successfully');</script>";
-			header('Location: index.php');
+			//header('Location: index.php');
 			
 		}
 	}
@@ -37,7 +39,14 @@
 <body>
 	<form method="post">
 		<label>Pincode:<input type="text" name="pin" value="<?php echo $pincode; ?>" class='text' disabled></label><br>
-		<label>Weight in grams:<input type="text" name="wt" class="text"></label><br>
+		<label>Weight:<br>
+			<input type="text" name="wt" class="text" required style="display: inline-block;">
+		</label>
+		<select name="wt-unit">
+			<option value="grams">grams</option>
+			<option value="kilograms">kilograms</option>
+		</select>
+		<br><br>
 		<input type="submit" name="sell-submit" value="Apply" id='submit'>
 		<input type="reset" name="reset" value="Reset" id="reset">
 	</form>
