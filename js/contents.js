ee629be5cb708adfea839cadf538397681002fdd
darshan <darshankhot@protonmail.com>
2018-11-03 21:56:13 +0530

    updated js to change button color on index page

--- js/contents.js
+++ js/contents.js
@@ -3,15 +3,29 @@ var about = document.getElementById('about-content');
 var contact = document.getElementById('contact-content');
 */
 
-function navBtn_click(btnId)
+function navBtn_click(navId,btnId)
 {
 	var home = document.getElementById("home-content");
 	var about = document.getElementById("about-content");
 	var contact = document.getElementById("contact-content");
 
+	var homeBtn = document.getElementById("home-button");
+	var aboutBtn = document.getElementById("about-button");
+	var contactBtn = document.getElementById("contact-button");
+
 	home.style.display = "none";
 	about.style.display = "none";
 	contact.style.display = "none";
-	btnId.style.display = "block";
+
+	homeBtn.style.backgroundColor = "#00b300";
+	homeBtn.style.color = "#ffffff";
+	aboutBtn.style.backgroundColor = "#00b300";
+	aboutBtn.style.color = "#ffffff";
+	contactBtn.style.backgroundColor = "#00b300";
+	contactBtn.style.color = "#ffffff";
+
+	btnId.style.backgroundColor = "#ffffff";
+	btnId.style.color = "#00b300";
+	navId.style.display = "block";
 	
 }
