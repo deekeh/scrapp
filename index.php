85a6f2aee11cb408c8bb8279011570952847a2b8
darshan <darshankhot@protonmail.com>
2018-11-06 21:49:47 +0530

    Added sample text to index.php with some styles in decorate.css

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 	<title>Scrapp</title>
 	<link rel="stylesheet" type="text/css" href="style/decorate.css">
 	<script type="text/javascript" src="js/contents.js"></script>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
 	<div id="menubar">
@@ -43,34 +44,60 @@
 	<div id="about"><input type="button" value="About" id="about-button" onclick="navBtn_click(getElementById('about-content'),getElementById('about-button'))"></button></div>
 	<div id="contact"><input type="button" value="Contact us" id="contact-button" onclick="navBtn_click(getElementById('contact-content'),getElementById('contact-button'))"></button></div>
 	<br><br>
+
+
 	<div id="home-content" style="display: block;">
 		<div>
 		<fieldset>
-			<img src="images/scrap.png" width="20%">
+			<img class="main-img-left" src="images/scrap.png" width="20%" />
+			<div class="img-info">
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc tincidunt ante vitae massa. Et harum quidem rerum facilis est et expedita distinctio. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nam sed tellus id magna elementum tincidunt. Phasellus rhoncus. Aliquam erat volutpat. Nullam eget nisl. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Vivamus porttitor turpis ac leo. Ut tempus purus at lorem. Maecenas aliquet accumsan leo. Mauris elementum mauris vitae tortor. Praesent vitae arcu tempor neque lacinia pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus et lorem id felis nonummy placerat.
+			</div>
+			</img>
 		</fieldset>
 		</div>
 	</div>
+
 	<div id="about-content" style="display: none;">
 		<div>
 		<fieldset>
-			<img src="images/recycle.png" width="20%">
+			<img class="main-img-left" src="images/recycle.png" width="20%">
+			<div class="img-info">
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc tincidunt ante vitae massa. Et harum quidem rerum facilis est et expedita distinctio. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nam sed tellus id magna elementum tincidunt. Phasellus rhoncus. Aliquam erat volutpat. Nullam eget nisl. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Vivamus porttitor turpis ac leo. Ut tempus purus at lorem. Maecenas aliquet accumsan leo. Mauris elementum mauris vitae tortor. Praesent vitae arcu tempor neque lacinia pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus et lorem id felis nonummy placerat.
+			</div>
+			</img>
 		</fieldset>
 		</div><br>
 		<div>
 		<fieldset>
-			<img src="images/apply.png" width="15%">
+			<img class="main-img-right" src="images/apply.png" width="15%" style="float: right;">
+			<div class="img-info">
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc tincidunt ante vitae massa. Et harum quidem rerum facilis est et expedita distinctio. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nam sed tellus id magna elementum tincidunt. Phasellus rhoncus. Aliquam erat volutpat. Nullam eget nisl. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Vivamus porttitor turpis ac leo. Ut tempus purus at lorem. Maecenas aliquet accumsan leo. Mauris elementum mauris vitae tortor. Praesent vitae arcu tempor neque lacinia pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus et lorem id felis nonummy placerat.
+			</div>
+			</img>
 		</fieldset>
 		</div><br>
 		<div>
 		<fieldset>
-			<img src="images/delivery.png" width="20%">
+			<img class="main-img-left" src="images/delivery.png" width="20%">
+			<div class="img-info">
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc tincidunt ante vitae massa. Et harum quidem rerum facilis est et expedita distinctio. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nam sed tellus id magna elementum tincidunt. Phasellus rhoncus. Aliquam erat volutpat. Nullam eget nisl. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Vivamus porttitor turpis ac leo. Ut tempus purus at lorem. Maecenas aliquet accumsan leo. Mauris elementum mauris vitae tortor. Praesent vitae arcu tempor neque lacinia pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus et lorem id felis nonummy placerat.
+			</div>
+			</img>
 		</fieldset>
 		</div>
 	</div>
+
 	<div id="contact-content" style="display: none;">
 		<div>
 		<fieldset>
-			<img src="images/contact.png" width="20%">
+			<img class="main-img-left" src="images/contact.png" width="20%">
+			<div class="img-info">
+				Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nunc tincidunt ante vitae massa. Et harum quidem rerum facilis est et expedita distinctio. Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Nam sed tellus id magna elementum tincidunt. Phasellus rhoncus. Aliquam erat volutpat. Nullam eget nisl. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Vivamus porttitor turpis ac leo. Ut tempus purus at lorem. Maecenas aliquet accumsan leo. Mauris elementum mauris vitae tortor. Praesent vitae arcu tempor neque lacinia pretium. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Phasellus et lorem id felis nonummy placerat.
+				<br><br>
+				<a href="mailto:mail @scrapp.com" style="text-decoration: none; color: #00b300;">Contact us</a>
+			</div>
+			</img>
 		</fieldset>
 		</div>
 	</div>
